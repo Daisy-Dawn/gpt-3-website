6c8ca43d08adc7bdf4059d62594eda26262429a0
Daisy-Dawn <agboella20@gmail.com>
2023-12-21 06:10:41 +0100

    project completed, fully responsive frontend landing page 🤩🥵💫👩‍💻

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,53 @@
 import './Navbar.css';
+import React, { useState } from 'react'
+import logo from '../../assets/logo.svg'
+import {RiMenu3Line, RiCloseLine} from 'react-icons/ri'
 
-import React from 'react'
+const Menu = () => (
+  <>
+    <p><a href="#home">Home</a></p>
+    <p><a href="#wgpt3">What is GPT?</a></p>
+    <p><a href="#possibility">Open AI</a></p>
+    <p><a href="#features">Case Studies</a></p>
+    <p><a href="#blog">Library</a></p>
+  </>
+);
+
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false)
   return (
-    <div>Navbar</div>
+    <div className='gpt3__navbar'>
+      <div className="gpt3__navbar-links">
+        <div className="gpt__navbar-links_logo">
+          <img src={logo} alt="" />
+        </div>
+        <div className="gpt3__navbar-link_container">
+        <Menu />
+        </div>
+      </div>
+      <div className='gpt3__navbar-sign'>
+        <p>Sign In</p>
+        <button type='button'>Sign Up</button>
+      </div>
+      <div className='gpt3__navbar-menu'>
+        {toggleMenu ? 
+          <RiCloseLine color='#fff' size={27} onClick={() => setToggleMenu(false)} /> 
+          : <RiMenu3Line color='#fff' size={27} onClick={() => setToggleMenu(true)} />
+        }
+        {toggleMenu && (
+          <div className='gpt3__navbar-menu_container scale-up-center'>
+            <div className='gpt3__navbar-menu_container-links'>
+              <Menu />
+              <div className='gpt3__navbar-menu-container-links-sign'>
+                <p>Sign In</p>
+                <button type='button'>Sign Up</button>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
   )
 }
 
